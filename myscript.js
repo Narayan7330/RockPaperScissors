dae1aba8f7f1ba4237af0ca7c770a5c3f62c1e91
Narayan7330 <narayanmali7330@gmail.com>
2025-04-05 11:52:16 +0000

    made some ui

--- myscript.js
+++ myscript.js
@@ -88,72 +88,72 @@ else if (humanChoice=== "scissors" && computerChoice === "paper"){
 
 
 
-//  logic to play the entire game (5 rounds)
-function playGame(){
+// //  logic to play the entire game (5 rounds)
+// function playGame(){
 
-let humanScore = 0;
-let computerScore = 0;
-
-
-// round1
-
-const humanSelection1 = getHumanChoice();
-const computerSelection1 = getComputerChoice();
-const winner1 = playRound(humanSelection1,computerSelection1);
-if (winner1 === "human") humanScore++;
-if (winner1 === "computer") computerScore++;
-
-// round2
-
-const humanSelection2 = getHumanChoice();
-const computerSelection2 = getComputerChoice();
-const winner2 = playRound(humanSelection2,computerSelection2);
-if (winner2 === "human") humanScore++;
-if (winner2 === "computer") computerScore++;
-
-// round3
-
-const humanSelection3 = getHumanChoice();
-const computerSelection3 = getComputerChoice();
-const winner3 = playRound(humanSelection3,computerSelection3);
-if (winner3 === "human") humanScore++;
-if (winner3 === "computer") computerScore++;
-
-// round4
-
-const humanSelection4 = getHumanChoice();
-const computerSelection4 = getComputerChoice();
-const winner4 = playRound(humanSelection4,computerSelection4);
-if (winner4 === "human") humanScore++;
-if (winner4 === "computer") computerScore++;
-
-// round5
-
-const humanSelection5 = getHumanChoice();
-const computerSelection5 = getComputerChoice();
-const winner5 = playRound(humanSelection5,computerSelection5);
-if (winner5 === "human") humanScore++;
-if (winner5 === "computer") computerScore++;
+// let humanScore = 0;
+// let computerScore = 0;
 
 
-// game end
-console.log("Game Ends");
-console.log("score are as follows");
-console.log(`you: ${humanScore}`);
-console.log(`computer: ${computerScore}`);
+// // round1
 
-if (humanScore > computerScore) {
+// const humanSelection1 = getHumanChoice();
+// const computerSelection1 = getComputerChoice();
+// const winner1 = playRound(humanSelection1,computerSelection1);
+// if (winner1 === "human") humanScore++;
+// if (winner1 === "computer") computerScore++;
 
-console.log("you won the game");
-}
-else if (humanScore < computerScore){
+// // round2
 
-  console.log("computer won the game");
-}
-else {
-console.log("game is tie");
-}
+// const humanSelection2 = getHumanChoice();
+// const computerSelection2 = getComputerChoice();
+// const winner2 = playRound(humanSelection2,computerSelection2);
+// if (winner2 === "human") humanScore++;
+// if (winner2 === "computer") computerScore++;
 
-}
+// // round3
+
+// const humanSelection3 = getHumanChoice();
+// const computerSelection3 = getComputerChoice();
+// const winner3 = playRound(humanSelection3,computerSelection3);
+// if (winner3 === "human") humanScore++;
+// if (winner3 === "computer") computerScore++;
+
+// // round4
+
+// const humanSelection4 = getHumanChoice();
+// const computerSelection4 = getComputerChoice();
+// const winner4 = playRound(humanSelection4,computerSelection4);
+// if (winner4 === "human") humanScore++;
+// if (winner4 === "computer") computerScore++;
+
+// // round5
+
+// const humanSelection5 = getHumanChoice();
+// const computerSelection5 = getComputerChoice();
+// const winner5 = playRound(humanSelection5,computerSelection5);
+// if (winner5 === "human") humanScore++;
+// if (winner5 === "computer") computerScore++;
+
+
+// // game end
+// console.log("Game Ends");
+// console.log("score are as follows");
+// console.log(`you: ${humanScore}`);
+// console.log(`computer: ${computerScore}`);
+
+// if (humanScore > computerScore) {
+
+// console.log("you won the game");
+// }
+// else if (humanScore < computerScore){
+
+//   console.log("computer won the game");
+// }
+// else {
+// console.log("game is tie");
+// }
+
+// }
 
 playGame();
